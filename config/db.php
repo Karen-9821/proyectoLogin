32225329f7f1dbf15e718d47191311a888ee734a
Karen Jimenez <ti17110175@itess.edu.mx>
2021-04-25 16:37:26 -0500

    Establecer conexion con la bd

--- config/db.php
+++ config/db.php
@@ -3,4 +3,4 @@
 define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'root');//Usuario de tu base de datos
 define('DB_PASS', '');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'sel_bd');//Nombre de la base de datos
+define('DB_NAME', 'login');//Nombre de la base de datos
