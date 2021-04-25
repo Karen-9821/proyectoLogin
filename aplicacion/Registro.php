594104d64bb8a86411f93f7d02e59a06c8c71cb9
Karen Jimenez <ti17110175@itess.edu.mx>
2021-04-25 16:15:54 -0500

    Creacion del formulario Registro

--- aplicacion/Registro.php
+++ aplicacion/Registro.php
@@ -8,8 +8,9 @@ header("location: INICIO.php");
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>REGISTRO ALUMNO</title>
+<title>REGISTRO</title>
 <!-- Custom Theme files -->
+<link rel="stylesheet" href="../public/CSS/bootstrap.min.css">
 <link href="../public/CSS/estilos.css" rel="stylesheet" type="text/css" media="all"/>
 <!-- for-mobile-apps -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -21,9 +22,22 @@ header("location: INICIO.php");
 <link href='//fonts.googleapis.com/css?family=Signika:400,600' rel='stylesheet' type='text/css'>
 <!--google fonts-->
 </head>
-<body>
+<body style="max-height: 0px;">
+
+    <div style="background-color: silver;" class="container-fluid">
+    <div style="background-color: skyblue;"  class="container" style="height: 100%;">
+
+        <header class="row">
+            <div class="col">
+                <nav class="navbar navbar-expand-lg navbarfixed-top navbar-dark bg-dark row">
+                    <a class="navbar-brand" href="#">
+                        <img src="../public/img/Login.png" width="80" height="80" alt="">
+                    </a>
+                </nav>
+            </div>
+        </header>
 <!--header start here-->
-<h1>Registrate para acceder al sistema</h1>
+<h1>Registrate para acceder</h1>
 <div class="header agile">
 	<div class="wrap">
 		<div class="login-main wthree">
@@ -31,9 +45,6 @@ header("location: INICIO.php");
 			<h3>REGISTRO</h3>
 			<form action="#" method="post">
 				<input type="text" placeholder="Nombre" required="" name="nombre" required>
-				<input type="text" placeholder="Apellidos" required="" name="apellidos" required>
-				<input type="text" placeholder="Institucion Eduativa" required="" name="institucion" required>
-                <input type="text" placeholder="Matricula" required="" name="matricula" required>
 				<input type="text" placeholder="Usuario" required="" name="username" required>
 				<input type="password" placeholder="Contraseña" name="password" required>
 				<input name="submit" type="submit" value="Registrar">
@@ -46,12 +57,24 @@ header("location: INICIO.php");
 		</div>
 	</div>
 </div>
-<!--header end here-->
-<!--copy rights end here-->
-<div class="copy-rights w3l">		 	
-	<p>© <?php echo date("Y");?> <a href="http://obedalvarado.pw/" target="_blank">Sistemas Web</a>  Todos los derechos reservados | Diseñado por  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>		 	
-</div>
+<footer class="row" style="background-color: #4B525F; margin-top: 50px; margin-bottom: 200px; height: 130px" >
+			<div class="col">
+				<div class="copy-rights w3l row">		 	
+					<p style="padding-top: 0px; text-align: center;">© <?php echo date("Y");?> <a href="http://obedalvarado.pw/" target="_blank">Taller de Investigacion II</a>  Todos los derechos reservados | Basado en el diseño de:  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>		 	
+				</div>
+			</div>
+		</footer>
+		
+	</div>
+	</div>
+
 <!--copyrights start here-->
- 
+   <script src="js/jquery-3.4.1.min.js"></script>
+  <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <script src="js/bootstrap.min.js"></script>
+  <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> -->
 </body>
 </html>
