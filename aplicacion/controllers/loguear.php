d575063c0e511af896b10430c9aa349b63bfc55a
Karen Jimenez <ti17110175@itess.edu.mx>
2021-04-26 08:04:16 -0500

    Cambiar archivo de carpeta

---
+++ aplicacion/controllers/loguear.php
@@ -0,0 +1,37 @@
+<?php
+
+include_once '../models/verificar_usuario.php';
+include_once '../models/sesion_usuario.php';
+
+$userSession = new UserSession();
+$user = new User();
+$tabla='usuario';
+
+
+if(isset($_SESSION['user'])){
+    $user->setUser($userSession->getCurrentUser(),$tabla);
+    include_once 'INICIO.php';
+
+}else if(isset($_POST['username']) && isset($_POST['password'])){
+
+    $userForm = $_POST['username'];
+    $passForm = $_POST['password'];
+
+    if($user->userExists($userForm, $passForm, $tabla)){
+        $userSession->setCurrentUser($userForm);
+        $user->setUser($userForm, $tabla);
+
+        include_once 'INICIO.php';
+        
+    }else{
+
+        $errorLogin = "Nombre de usuario y/o password es incorrecto";
+        include_once 'iniciarSesion.php';
+
+    }
+
+}else{
+
+    include_once 'iniciarSesion.php';
+}
+?>
